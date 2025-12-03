145465825a07ef08e42c9b83821c953855c30fbb
JaneTran91 <91128289+JaneTran91@users.noreply.github.com>
2025-12-03 17:26:24 +0700

    ok

---
+++ story_content/user.js
@@ -0,0 +1,243 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ZLjTJHVfcg":
+        Script1();
+        break;
+      case "5qkEvcbpN8l":
+        Script2();
+        break;
+      case "6YrRIaHiFsV":
+        Script3();
+        break;
+      case "67lbAqOtowX":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  function add_script(scriptURL,oID) {
+     var scriptEl = document.createElement("script");
+     var head=document.getElementsByTagName('head')[0];
+      scriptEl.type = "text/javascript";      
+      scriptEl.src = scriptURL;      
+      scriptEl.id=oID;      
+      head.appendChild(scriptEl);}
+
+//only want to add these once!
+if(document.getElementById('jquery')==null){
+add_script("https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js","jquery");
+
+}
+
+
+/* Get into full screen */
+function GoInFullscreen(preso) {
+	if(preso.requestFullscreen)
+		preso.requestFullscreen();
+	else if(preso.mozRequestFullScreen)
+		preso.mozRequestFullScreen();
+	else if(preso.webkitRequestFullscreen)
+		preso.webkitRequestFullscreen();
+	else if(preso.msRequestFullscreen)
+		preso.msRequestFullscreen();
+}
+
+/* Get out of full screen */
+function GoOutFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
+}
+
+/* Is currently in full screen or not */
+function IsFullScreenCurrently() {
+	var full_screen_preso = document.fullscreenpreso || document.webkitFullscreenpreso || document.mozFullScreenpreso || document.msFullscreenpreso || null;
+	
+	// If no preso is in full-screen
+	if(full_screen_preso === null)
+		return false;
+	else
+		return true;
+}
+
+GoOutFullscreen();
+
+}
+
+function Script2()
+{
+  function add_script(scriptURL,oID) {
+     var scriptEl = document.createElement("script");
+     var head=document.getElementsByTagName('head')[0];
+      scriptEl.type = "text/javascript";      
+      scriptEl.src = scriptURL;      
+      scriptEl.id=oID;      
+      head.appendChild(scriptEl);}
+
+//only want to add these once!
+if(document.getElementById('jquery')==null){
+add_script("https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js","jquery");
+
+}
+
+
+/* Get into full screen */
+function GoInFullscreen(preso) {
+	if(preso.requestFullscreen)
+		preso.requestFullscreen();
+	else if(preso.mozRequestFullScreen)
+		preso.mozRequestFullScreen();
+	else if(preso.webkitRequestFullscreen)
+		preso.webkitRequestFullscreen();
+	else if(preso.msRequestFullscreen)
+		preso.msRequestFullscreen();
+}
+
+/* Get out of full screen */
+function GoOutFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
+}
+
+/* Is currently in full screen or not */
+function IsFullScreenCurrently() {
+	var full_screen_preso = document.fullscreenpreso || document.webkitFullscreenpreso || document.mozFullScreenpreso || document.msFullscreenpreso || null;
+	
+	// If no preso is in full-screen
+	if(full_screen_preso === null)
+		return false;
+	else
+		return true;
+}
+
+GoInFullscreen($("#preso").get(0));
+
+}
+
+function Script3()
+{
+  function add_script(scriptURL,oID) {
+     var scriptEl = document.createElement("script");
+     var head=document.getElementsByTagName('head')[0];
+      scriptEl.type = "text/javascript";      
+      scriptEl.src = scriptURL;      
+      scriptEl.id=oID;      
+      head.appendChild(scriptEl);}
+
+//only want to add these once!
+if(document.getElementById('jquery')==null){
+add_script("https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js","jquery");
+
+}
+
+
+/* Get into full screen */
+function GoInFullscreen(preso) {
+	if(preso.requestFullscreen)
+		preso.requestFullscreen();
+	else if(preso.mozRequestFullScreen)
+		preso.mozRequestFullScreen();
+	else if(preso.webkitRequestFullscreen)
+		preso.webkitRequestFullscreen();
+	else if(preso.msRequestFullscreen)
+		preso.msRequestFullscreen();
+}
+
+/* Get out of full screen */
+function GoOutFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
+}
+
+/* Is currently in full screen or not */
+function IsFullScreenCurrently() {
+	var full_screen_preso = document.fullscreenpreso || document.webkitFullscreenpreso || document.mozFullScreenpreso || document.msFullscreenpreso || null;
+	
+	// If no preso is in full-screen
+	if(full_screen_preso === null)
+		return false;
+	else
+		return true;
+}
+
+GoInFullscreen($("#preso").get(0));
+
+}
+
+function Script4()
+{
+  function add_script(scriptURL,oID) {
+     var scriptEl = document.createElement("script");
+     var head=document.getElementsByTagName('head')[0];
+      scriptEl.type = "text/javascript";      
+      scriptEl.src = scriptURL;      
+      scriptEl.id=oID;      
+      head.appendChild(scriptEl);}
+
+//only want to add these once!
+if(document.getElementById('jquery')==null){
+add_script("https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js","jquery");
+
+}
+
+
+/* Get into full screen */
+function GoInFullscreen(preso) {
+	if(preso.requestFullscreen)
+		preso.requestFullscreen();
+	else if(preso.mozRequestFullScreen)
+		preso.mozRequestFullScreen();
+	else if(preso.webkitRequestFullscreen)
+		preso.webkitRequestFullscreen();
+	else if(preso.msRequestFullscreen)
+		preso.msRequestFullscreen();
+}
+
+/* Get out of full screen */
+function GoOutFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
+}
+
+/* Is currently in full screen or not */
+function IsFullScreenCurrently() {
+	var full_screen_preso = document.fullscreenpreso || document.webkitFullscreenpreso || document.mozFullScreenpreso || document.msFullscreenpreso || null;
+	
+	// If no preso is in full-screen
+	if(full_screen_preso === null)
+		return false;
+	else
+		return true;
+}
+
+GoInFullscreen($("#preso").get(0));
+
+}
+
